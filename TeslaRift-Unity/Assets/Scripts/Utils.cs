a1b8a8c428289fae8e82e971de3cc548310d2eca
Mystfit <byronated@gmail.com>
2013-09-03 23:31:59 +1200

    Added catmull rom functions

--- TeslaRift-Unity/Assets/Scripts/Utils.cs
+++ TeslaRift-Unity/Assets/Scripts/Utils.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System;
+using System.Collections.Generic;
 
 
 public static class Utils
@@ -47,6 +48,55 @@ public static class Utils
 		}
 
 		return pts;
+	}
+	
+	/// <summary>
+	/// Takes an array of input coordinates used to define a Catmull-Rom spline, and then
+	/// samples the resulting spline according to the specified sample count (per span),
+	/// populating the output array with the newly sampled coordinates. The returned boolean
+	/// indicates whether the operation was successful (true) or not (false).
+	/// NOTE: The first and last points specified are used to describe curvature and will be dropped
+	/// from the resulting spline. Duplicate them if you wish to include them in the curve.
+	/// </summary>
+	public static bool CatmullRom(Vector3[] inCoordinates, out Vector3[] outCoordinates, int samples)
+	{
+		if (inCoordinates.Length < 4)
+		{
+			outCoordinates = null;
+			return false;
+		}
+	 
+		List<Vector3> results = new List<Vector3>();
+	 
+		for (int n = 1; n < inCoordinates.Length - 2; n++)
+			for (int i = 0; i < samples; i++)
+				results.Add(PointOnCurve(inCoordinates[n - 1], inCoordinates[n], inCoordinates[n + 1], inCoordinates[n + 2], (1f / samples) * i ));
+	 
+		results.Add(inCoordinates[inCoordinates.Length - 2]);
+	 
+		outCoordinates = results.ToArray();
+		return true;
+	}
+	 
+	/// <summary>
+	/// Return a point on the curve between P1 and P2 with P0 and P4 describing curvature, at
+	/// the normalized distance t.
+	/// </summary>
+	 
+	public static Vector3 PointOnCurve(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+	{
+		Vector3 result = new Vector3();
+	 
+		float t0 = ((-t + 2f) * t - 1f) * t * 0.5f;
+		float t1 = (((3f * t - 5f) * t) * t + 2f) * 0.5f;
+		float t2 = ((-3f * t + 4f) * t + 1f) * t * 0.5f;
+		float t3 = ((t - 1f) * t * t) * 0.5f;
+	 
+		result.x = p0.x * t0 + p1.x * t1 + p2.x * t2 + p3.x * t3;
+		result.y = p0.y * t0 + p1.y * t1 + p2.y * t2 + p3.y * t3;
+		result.z = p0.z * t0 + p1.z * t1 + p2.z * t2 + p3.z * t3;
+	 
+		return result;
 	}
 }
 
